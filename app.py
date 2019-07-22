fca3af803e04594c0756db1a2eb0022760483dd4
sonusharma07 <meetsonusharma07@gmail.com>
2019-07-22 12:14:16 +0530

    Add render_template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world"
+    title='Learn Flask'
+    return render_template('index.html' , title=title)
 
-    
