ead5595ad4cc2481569ef701ffc3dbb3548d3ab5
sonusharma07 <meetsonusharma07@gmail.com>
2019-07-24 10:26:45 +0530

    added SASS capability

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ def hello_world():
     ]
     return render_template('index.html' , title=title, programming_languages=programming_languages)
 
+@app.route('/about')
+def about():
+    title = 'about us'
+    return render_template('about.html' , title=title)
+
+@app.route('/contact')
+def contact():
+    title = 'contact us'
+    return render_template('contact.html' , title=title)
+
+@app.route('/terms')
+def terms():
+    title = 'Terms of Use'
+    return render_template('terms.html' , title=title)
+
+@app.route('/privacy')
+def privacy():
+    title = 'Privacy policy'
+    return render_template('privacy.html' , title=title)
